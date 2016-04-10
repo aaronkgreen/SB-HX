12fd8d5dda3455419de4e881c414a0ab02bfac84
Aaron K Green <aaron.k.green@btinternet.com>
2016-04-10 09:22:27 +0100

    Adjustments and Corrections
    Corrected some errors and brought in-line with my original design.

--- CrudAPI.php
+++ CrudAPI.php
@@ -1,19 +1,46 @@
 <?php
 // 09/04/2016 - File created.
 
-class CCrud
+abstract class CCrud
 {
-	// We need to know which CRUD state we're getting from the HTTP request.
-    private $gHttpReqMethod = '';
+	// Global variable for the HTTP CRUD state.
+    protected $gHttpReqMethod = '';
 	
-	// A simple constructor, I will need to develop this further for security reasons.
+	// Global variable for the model in the url.
+	protected $gModel = '';
+	
+	// Global variable for expanded data on the model in the url. 
+	protected $gExpModel = '';
+
+	// Global variable for an array of arguements in the url.
+	protected $argArray = Array();
+	
+	// A constructor that enables cross-origin sharing and houses the abstract functionality of the CRUD.
     public function __construct( $urlReq ) {
-		$this->UpdateHTTPReqMethod();
-		$this->CRUDTest( $urlReq );
+		
+		// Research following a bug suggests that adding this code allows for "cross-origin" sharing.
+		header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Methods: *");
+        header("Content-Type: application/json");
+		
+		$this->_ReasignURL( $urlReq );
+		$this->_SelectCRUDState();
+		$this->_DoURLSanitization( $urlReq );
     }
+
+	// Take the URL, break it down and reasign to variables as required.
+	private function _ReasignURL( $urlReq ) {
+		
+        $this->argArray = explode('/', rtrim( $urlReq, '/' ) );
+        $this->gModel = array_shift( $this->argArray );
+		
+        if ( array_key_exists( 0, $this->argArray) && !is_numeric( $this->argArray[ 0 ] ) ) {
+            $this->gExpModel = array_shift( $this->argArray );
+        }
+	}
 	
-	// Work out the CRUD state from the http request method.
-	private function UpdateHTTPReqMethod() {
+	// Work out the correct CRUD state from the http request method.
+	private function _SelectCRUDState() {
 	    $this->gHttpReqMethod = $_SERVER[ 'REQUEST_METHOD' ];
         if ( $this->gHttpReqMethod == 'POST' ) {	
 
@@ -26,14 +53,25 @@ class CCrud
         }	
 	}
 	
-	// Test that the Create, Read, Update and Delete requests pick up. 
-    private function CRUDTest( $urlReq ) {
+	// Take the URL and sanitize it for a smoother process going forward. 
+    private function _DoURLSanitization( $urlReq ) {
 
-        switch( gHttpReqMethod ) {
+        switch( this->gHttpReqMethod ) {
 				
 		// Create
 		case 'POST':
-			echo "Post request";
+			echo "Post request";	
+			$input = Array();
+			
+			if ( is_array( $_POST ) ) {
+				foreach( $_POST as $arraykey => $arrayVal ) {
+					// Not 100% sure about how to achieve what I want here.  I want to sanatize but how?
+					// 	will need to go away and ponder or do some research.
+				}
+			} else {
+				$input = trim( strip_tags( $_POST ) );
+			}
+			
 			break;
 				
 		// Read
@@ -46,7 +84,7 @@ class CCrud
 			echo "put request";
 			break;
 			
-		// See below...
+		// Delete
 		case 'DELETE':
 			echo "delete request";
 			break;
